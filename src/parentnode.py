80241d84b75c4346dd418b3faa6b9cb32a813a12
maczilla <bogdan.d@outlook.com>
2025-10-30 09:30:00 +0100

    parent node and it's test added

---
+++ src/parentnode.py
@@ -0,0 +1,20 @@
+from htmlnode import HTMLNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("ParentNode needs a tag value!")
+        if not self.children:
+            raise ValueError("ParentNode needs children!")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
